c2dfccaf334c8b0fcfd5959436d8679442ffb299
fadli <fadlirahmanfauzan04@gmail.com>
2025-03-10 21:32:07 +0700

    feat : event details

--- src/navigation/stacks/DashboardStackNavigator.js
+++ src/navigation/stacks/DashboardStackNavigator.js
@@ -1,13 +1,15 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { SCREEN_PATH } from "../PathNavigator";
 import DashboardScreen from "../../screens/dashboard/DashboardScreen";
+import EventDetailScreen from "../../screens/dashboard/event-detail/EventDetailScreen";
 
 const Stack = createNativeStackNavigator();
 
 const DashboardStackNavigator = () => {
     return (
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name={SCREEN_PATH.DASHBOARD} component={DashboardScreen} />            
+            <Stack.Screen name={SCREEN_PATH.DASHBOARD} component={DashboardScreen} />   
+            <Stack.Screen name={SCREEN_PATH.EVENT_DETAIL} component={EventDetailScreen} />         
         </Stack.Navigator>
     );
 };
--- src/screens/dashboard/events/EventCarousel.js
+++ src/screens/dashboard/events/EventCarousel.js
@@ -1,8 +1,11 @@
 import React from "react";
-import { View, Text, FlatList, Image } from "react-native";
+import { View, Text, FlatList, Image, TouchableOpacity } from "react-native";
 import styles from "./EventCarouse.style";
+import { SCREEN_PATH } from "../../../navigation/PathNavigator";
+import { useNavigation } from "@react-navigation/native";
 
 const EventCarousel = ({events}) => {
+    const navigation = useNavigation()
   return (
     <View>
       <View style={styles.sectionHeader}>
@@ -15,9 +18,12 @@ const EventCarousel = ({events}) => {
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.eventListContainer}
         renderItem={({ item }) => (
-          <View style={styles.eventCard}>
-            <Image source={{ uri: item.image }} style={styles.eventImage} />
-          </View>
+            <TouchableOpacity 
+              style={styles.eventCard} 
+              onPress={() => navigation.navigate(SCREEN_PATH.EVENT_DETAIL, { event: item })}
+            >
+              <Image source={{ uri: item.image }} style={styles.eventImage} />
+            </TouchableOpacity>
         )}
       />
     </View>
